55ea9c4a8607741938504d502ad0881b9b56ef4b
mattperkins <github@mattperkins.com>
2018-07-26 23:16:27 +0100

    add GQL typeDefs

--- schema.js
+++ schema.js
@@ -1,1 +1,20 @@
-exports.typeDefs = ``
+// GQL refers to schemas as typeDefs
+// models === types (converted to GQL syntax)
+exports.typeDefs = `
+    type Software {
+        name: String!
+        category: String!
+        description: String!
+        instructions: String!
+        createdDate: String
+        likes: Int
+        uername: String
+    }
+    type User {
+        username: String! @unique
+        password: String!
+        email: String!
+        joinDate: String
+        favourites: [Software]
+    }
+ `
