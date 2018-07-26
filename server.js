a20d16fbfae39778c26ecac1ae600cbdc967f80c
mattperkins <github@mattperkins.com>
2018-07-26 21:24:55 +0100

    comment code

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose')
 
 require('dotenv').config({ path: 'variables.env' })
 
-// connect to db
+// Connect to db
 mongoose
     .connect(process.env.MONGO_URI, { useNewUrlParser: true })
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err))
 
 
+// Initialise application
 const app = express()
 
 const PORT = process.env.PORT || 4444
