4392ebd74ccc1cd937d94b18e127bc8eb9bc0dd1
mattperkins <github@mattperkins.com>
2018-07-26 21:22:15 +0100

    connect to db

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config({ path: 'variables.env' })
+
+// connect to db
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err))
+
 
 const app = express()
 
