60c939cae39cd7d0bb67b476f0d07929ec2aa0cf
mattperkins <github@mattperkins.com>
2018-07-29 00:58:15 +0100

    add apollo-boost and configure client for graphql

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,17 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4444/graphql'
+})
+
+const root = document.getElementById('root')
+ReactDOM.render( 
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    root 
+)
