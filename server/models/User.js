8e8921069de27a56a1a132317392323b58e4f36d
mattperkins <github@mattperkins.com>
2018-07-29 12:44:49 +0100

    hash User password with bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
     username: {
@@ -25,5 +25,22 @@ const UserSchema = new Schema({
         ref: 'Software'
     }
 })
+// runs before user is saved to db
+UserSchema.pre('save', function (next) {
+    // if the password field on schema is not modified return 'next' (call the next function)
+    if (!this.isModified('password')) {
+        return next()
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err)
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if (err) return next(err)
+            this.password = hash
+            next()
+        })
+    })
+})
 
 module.exports = mongoose.model('User', UserSchema)
+
+
