a09e1f51816b97efb2af7b468f4821196a868de0
mattperkins <github@mattperkins.com>
2018-07-29 16:42:38 +0100

    configure form inputs state

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
 class Signup extends React.Component {
+
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
+        const { username, email, password, passwordConfirmation } = this.state
         return (
             <div className="center">
                 <h2 style={{ marginTop: 30, fontWeight: 900 }}>Signup</h2>
@@ -9,18 +25,26 @@ class Signup extends React.Component {
                     <input type="text"
                         name="username"
                         placeholder="Username"
+                        value={username}
+                        onChange={this.handleChange}
                     />
                     <input type="email"
                         name="email"
                         placeholder="Email Address"
+                        value={email}
+                        onChange={this.handleChange}
                     />
                     <input type="password"
                         name="password"
                         placeholder="Password"
+                        value={password}
+                        onChange={this.handleChange}
                     />
                     <input type="password"
                         name="passwordConfirmation"
                         placeholder="Confirm Password"
+                        value={passwordConfirmation}
+                        onChange={this.handleChange}
                     />
                     <button
                         type="submit"
